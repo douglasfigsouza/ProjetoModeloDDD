2e5906f36d7eae8f8d6cfa39ba9bcf5fed36f5a3
Douglas <douglasfigsouza@gmail.com>
2017-09-25 17:45:47 -0300

    Commit Inicial

---
+++ ProjetoModeloDDD.Domain/Entities/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProjetoModeloDDD.Domain.Entities
+{
+    public class Cliente
+    {
+        public int clienteId { get; set; }
+        public string nomeCli { get; set; }
+        public string sobrenomeCli { get; set; }
+        public DateTime dataNascimentoCli { get; set; }
+        public bool ativoCli { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente) {
+
+            return cliente.ativoCli && DateTime.Now.Year - cliente.dataNascimentoCli.Year >= 5;
+        }
+    }
+}
